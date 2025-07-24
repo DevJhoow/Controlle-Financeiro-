988f96487cd8a44718a7ee8fbfc858d7098445a8
JonathanLuis <jdk99.rodrigues@gmail.com>
2025-07-24 19:52:12 -0300

    secure_asset('css/paginaInicial.css')

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title> @yield('title') </title>
     {{-- css --}}
-    <link rel="stylesheet" href="{{ asset('css/paginaInicial.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/paginaInicial.css') }}">
 
     <!-- Bootstrap + Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
